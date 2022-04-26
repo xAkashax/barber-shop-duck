281930720e346df1c7a9e339a1744df6af430426
xAkashax <kbilan1992@gmail.com>
2022-04-26 23:28:59 +0200

    issues fix

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,23 @@
 //Nav toggle - select button and link
-const navToggle = document.querySelector('#nav-toggle')
-const nav = document.querySelector("#nav-links")
+const navToggle = document.querySelector('#nav-toggle');
+const nav = document.querySelector("#nav-links");
 
 navToggle.addEventListener("click", () => {
-    nav.classList.toggle('nav-open')
+    nav.classList.toggle('nav-open');
 })
 
 //Slider
+document.addEventListener("DOMContentLoaded", () => {
+    this.slider();
+});
+
 const slideImg = document.querySelector("#slide-img");
-const images = new Array(
+const images = [
     'images/header_1.jpg',
     'images/header_2.jpg',
     'images/header_3.jpg',
-    'images/header_4.jpg'
-);
+    'images/header_4.jpg',
+];
 
 const len = images.length;
 var i = 0;
@@ -24,7 +28,9 @@ function slider() {
     }
     slideImg.src = images[i];
     i++;
-    setTimeout('slider()', 3000);
+    setTimeout(() => {
+        this.slider()
+    }, 6000);
 }
 
 //ScrollRevall
@@ -33,7 +39,6 @@ ScrollReveal({
     distance: '60px',
     duration: 1500,
     delay: 300,
-
 });
 
 ScrollReveal().reveal('.content-banner, h1, .about-us-description, .desc_serv, .price-btn', {
@@ -42,18 +47,18 @@ ScrollReveal().reveal('.content-banner, h1, .about-us-description, .desc_serv, .
     interval: 200,
     opacity: 0.1
 });
-ScrollReveal().reveal('.logos-item, .btn-social, .contact', {
-    delay: 200,
+ScrollReveal().reveal('.logos-item, .btn-social', {
+    delay: 100,
     origin: 'bottom',
-    interval: 300,
+    interval: 200,
     mobile: false
 });
 
 //Scroll up
 
-const scrollUp = document.querySelector('.scroll-up')
+const scrollUp = document.querySelector('.scroll-up');
 
-window / addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
     if (window.pageYOffset > 100) {
         scrollUp.classList.add('active');
     } else {
